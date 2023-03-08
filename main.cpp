2d96fbe92f70c021d905c6dde24cc48401e54a40
LopezEloy <104275355+LopezEloy@users.noreply.github.com>
2023-03-07 20:16:09 -0600

    Main Code to start the program

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+
+#include "start.h"
+#include "parkingLot.h"
+#include "Admin.h"
+#include "ManagementEmployee.h"
+#include "employee.h"
+
+using namespace std;
+
+int main(){
+
+    start();
+
+    return 0;
+}
